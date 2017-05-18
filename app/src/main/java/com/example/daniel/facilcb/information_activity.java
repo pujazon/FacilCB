84ca1da4e20d2112935c3f216bf54525b0302779
Daniel <danielpujazon1899@gmail.com>
2017-05-18 14:53:41 +0200

    Applicacio de movil IPS Camp Nou

---
+++ app/src/main/java/com/example/daniel/facilcb/information_activity.java
@@ -0,0 +1,15 @@
+package com.example.daniel.facilcb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class information_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+
+    }
+}
