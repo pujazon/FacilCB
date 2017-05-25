49078a3224d037f19042da3c7251e7524594186e
Daniel <danielpujazon1899@gmail.com>
2017-05-25 09:02:57 +0200

    IPS Imageviewer

---
+++ app/src/main/java/com/example/daniel/facilcb/BlueDotView.java
@@ -0,0 +1,64 @@
+package com.example.daniel.facilcb;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
+/**
+ * Created by daniel on 25/05/17.
+ */
+
+public class BlueDotView extends SubsamplingScaleImageView {
+
+    private float radius = 1.f;
+    private PointF dotCenter = null;
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    public void setDotCenter(PointF dotCenter) {
+        this.dotCenter = dotCenter;
+    }
+
+    public BlueDotView(Context context) {
+        this(context, null);
+    }
+
+    public BlueDotView(Context context, AttributeSet attr) {
+        super(context, attr);
+        initialise();
+    }
+
+    private void initialise() {
+        setWillNotDraw(false);
+        setPanLimit(SubsamplingScaleImageView.PAN_LIMIT_CENTER);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (!isReady()) {
+            return;
+        }
+
+        if (dotCenter != null) {
+            PointF vPoint = sourceToViewCoord(dotCenter);
+            float scaledRadius = getScale() * radius;
+            Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(getResources().getColor(R.color.cast_expanded_controller_background_color));
+            canvas.drawCircle(vPoint.x, vPoint.y, scaledRadius, paint);
+        }
+    }
+
+
+
+}
